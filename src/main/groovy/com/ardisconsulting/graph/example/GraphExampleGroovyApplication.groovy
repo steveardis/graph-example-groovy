53255c22cab7619932d6227420806b5647ed489e
Steve Ardis <scardis@gmail.com>
2020-11-25 11:31:31 -0500

    Initial commit

---
+++ src/main/groovy/com/ardisconsulting/graph/example/GraphExampleGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package com.ardisconsulting.graph.example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GraphExampleGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GraphExampleGroovyApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/ardisconsulting/graph/example/GraphExampleGroovyApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.ardisconsulting.graph.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphExampleGroovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
