b0e0c97ba4b83acb52dfb5bbaa4dbdbd955d5c48
Steve Ardis <scardis@gmail.com>
2020-11-25 11:42:09 -0500

    Added @CompileStatic to existing classes

--- src/main/groovy/com/ardisconsulting/graph/example/GraphExampleGroovyApplication.groovy
+++ src/main/groovy/com/ardisconsulting/graph/example/GraphExampleGroovyApplication.groovy
@@ -1,8 +1,10 @@
 package com.ardisconsulting.graph.example
 
+import groovy.transform.CompileStatic
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+@CompileStatic
 @SpringBootApplication
 class GraphExampleGroovyApplication {
 
--- src/test/groovy/com/ardisconsulting/graph/example/GraphExampleGroovyApplicationTests.groovy
+++ src/test/groovy/com/ardisconsulting/graph/example/GraphExampleGroovyApplicationTests.groovy
@@ -1,8 +1,10 @@
 package com.ardisconsulting.graph.example
 
+import groovy.transform.CompileStatic
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+@CompileStatic
 @SpringBootTest
 class GraphExampleGroovyApplicationTests {
 
